7eeb0c8bed0aa2eab52a4ab54b7c424db9f05d79
Toskopiya21 <marikyan.tosya@mail.ru>
2026-06-01 23:29:12 +0300

    feat: add eslint-boundaries, prettier

---
+++ eslint.boundaries.js
@@ -0,0 +1,67 @@
+import boundaries from "eslint-plugin-boundaries";
+
+export const eslintBoundariesConfig = {
+  plugins: {
+    boundaries,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+
+    "boundaries/elements": [
+      {
+        type: "app",
+        pattern: "./src/app",
+      },
+      {
+        type: "features",
+        pattern: "./src/features/*",
+      },
+      {
+        type: "shared",
+        pattern: "./src/shared",
+      },
+    ],
+  },
+  rules: {
+    "boundaries/dependencies": [
+      2,
+      {
+        default: "allow",
+        rules: [
+          {
+            from: { type: "shared" },
+            disallow: {
+              to: {
+                type: ["app", "features"],
+              },
+            },
+            message:
+              "The module of the underlying layer (${file.type}) cannot import the module of the overlying layer (${dependency.type})",
+          },
+          {
+            from: { type: "features" },
+            disallow: {
+              to: {
+                type: ["app"],
+              },
+            },
+            message:
+              "The module of the underlying layer (${file.type}) cannot import the module of the overlying layer (${dependency.type})",
+          },
+          {
+            to: { type: "features", internalPath: "!index.(ts|tsx)" },
+            disallow: {
+              from: { type: "*" },
+            },
+            message:
+              "The module (${file.type}) must be imported via the public API. Direct import from ${dependency.source} is prohibited",
+          },
+        ],
+      },
+    ],
+  },
+};
--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import { eslintBoundariesConfig } from "./eslint.boundaries.js";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+export default defineConfig(
+  [
+    globalIgnores(["dist"]),
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs.flat.recommended,
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        globals: globals.browser,
+      },
     },
-  },
-])
+  ],
+  eslintBoundariesConfig,
+);
